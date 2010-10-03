782186a782c5611bad2d479df2847f9c6ced1053
Justin Davis <jrcd83@gmail.com>
2010-10-03 08:27:24 -0700

    Add documentation for WWW::AUR::Iterator.

--- lib/WWW/AUR/Iterator.pm
+++ lib/WWW/AUR/Iterator.pm
@@ -88,3 +88,142 @@ sub next
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+WWW::AUR::Iterator - An iterator for looping through all AUR packages.
+
+=head1 SYNOPSIS
+
+  my $iter = $aurobj->packages;
+  # or my $iter = WWW::AUR::Iterator->new();
+
+  while ( my $pkg = $iter->next ) {
+      print $pkg->name, "\n";
+  }
+
+  $iter->reset;
+  while ( my $pkgname = $iter->next_name ) {
+      print "$pkgname\n";
+  }
+
+=head1 DESCRIPTION
+
+A B<WWW::AUR::Iterator> object can be used to iterate through I<all>
+packages currently listed on the AUR webiste.
+
+=head1 CONSTRUCTOR
+
+  $OBJ = WWW::AUR::Iterator->new( %PATH_PARAMS );
+
+=over 4
+
+=item Parameters
+
+The parameters are the same as the L<WWW::AUR> constructor. These are
+propogated to any L<WWW::AUR::Package> objects that are created.
+
+=back
+
+=over 4
+
+=item Returns
+
+=over 4
+
+=item C<$OBJ>
+
+A L<WWW::AUR::Iterator> object.
+
+=back
+
+=back
+
+=head1 METHODS
+
+=head2 reset
+
+  $OBJ->reset;
+
+The iterator is reset to the beginning of all packages available in
+the AUR. This starts the iteration over just like creating a new
+I<WWW::AUR::Iterator> object.
+
+=head2 next
+
+  $PKGOBJ | undef = $OBJ->next();
+
+=over 4
+
+=item Returns
+
+=over 4
+
+=item C<$PKGOBJ>
+
+A L<WWW::AUR::Package> object representing the next package in the AUR.
+
+=item I<undef>
+
+If we have iterated through all packages, then I<undef> is returned.
+
+=back
+
+=back
+
+=head2 next_name
+
+  $PKGNAME | undef = $OBJ->next_name();
+
+=over 4
+
+=item Returns
+
+=over 4
+
+=item C<$PKGNAME>
+
+The name of the next package in the AUR. This is faster than
+L<METHODS/next> because L<WWW::AUR::Package> objects do not have to be
+created for every package on the AUR.
+
+=item I<undef>
+
+If we have iterated through all packages, then I<undef> is returned.
+
+=back
+
+=back
+
+=head1 SEE ALSO
+
+L<WWW::AUR>
+
+=head1 AUTHOR
+
+Justin Davis, C<< <juster at cpan dot org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-www-aur at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-AUR>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+Send me an email at the above address if you have any questions or
+need help.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Justin Davis.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
