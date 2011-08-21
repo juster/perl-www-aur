f09d30aaf05d9271585e3bee670e10581d3e793a
Justin Davis <jrcd83@gmail.com>
2011-08-21 13:06:33 -0400

    Add $WWW::AUR::URI::Scheme to allow overriding to https.

--- lib/WWW/AUR/URI.pm
+++ lib/WWW/AUR/URI.pm
@@ -12,6 +12,7 @@ use WWW::AUR qw(); # for global variables
 our @ISA         = qw(Exporter);
 our @EXPORT_OK   = qw(pkgfile_uri pkgbuild_uri pkg_uri rpc_uri);
 our %EXPORT_TAGS = ( 'all' => [ @EXPORT_OK ] );
+our $Scheme      = 'http';
 
 sub _pkgdir
 {
@@ -24,20 +25,21 @@ sub pkgfile_uri
 {
     my ($pkgname) = @_;
     my $dir = _pkgdir($pkgname);
-    return "http://$WWW::AUR::HOST/$dir/$pkgname.tar.gz";
+    return "$Scheme://$WWW::AUR::HOST/$dir/$pkgname.tar.gz";
 }
 
 sub pkgbuild_uri
 {
     my ($pkgname) = @_;
     my $dir = _pkgdir($pkgname);
-    return "http://$WWW::AUR::HOST/$dir/PKGBUILD"
+    return "$Scheme://$WWW::AUR::HOST/$dir/PKGBUILD"
 }
 
 sub pkg_uri
 {
     my %params = @_;
     my $scheme = delete $params{'https'} ? 'https' : 'http';
+    $scheme  ||= $Scheme;
     my $uri    = URI->new( "$scheme://$WWW::AUR::HOST/packages.php" );
     $uri->query_form([ %params ]);
     return $uri->as_string;
@@ -52,7 +54,7 @@ sub rpc_uri
     Carp::croak( "$method is not a valid AUR RPC method" )
         unless grep { $_ eq $method } @_RPC_METHODS;
 
-    my $uri = URI->new( "http://$WWW::AUR::HOST/rpc.php" );
+    my $uri = URI->new( "$Scheme://$WWW::AUR::HOST/rpc.php" );
 
     my @qparms = ( 'type' => $method );
     if ($method eq 'minfo') {
