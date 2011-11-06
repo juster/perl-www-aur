ebb2084ffe1912a6b3eb703fd9827ea7a779833b
Justin Davis <jrcd83@gmail.com>
2011-11-06 14:14:31 -0500

    Add InitTLS class method to UserAgent and fix version string.

--- lib/WWW/AUR/UserAgent.pm
+++ lib/WWW/AUR/UserAgent.pm
@@ -11,7 +11,16 @@ our @ISA = qw(LWP::UserAgent);
 sub new
 {
     my $class = shift;
-    $class->SUPER::new( agent => $WWW::AUR::USERAGENT, @_ );
+
+    $class->SUPER::new( 'agent' => "WWW::AUR/v$WWW::AUR::VERSION", @_ );
+}
+
+#---CLASS METHOD---
+sub InitTLS
+{
+    eval { require LWP::Protocol::https; 1 } or
+        die "failed to load LWP::Protocol::https, error:\n$@\n"
+            . "(ensure that LWP::Protocol::https is installed)\n"
 }
 
 1;
